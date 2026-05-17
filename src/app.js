8a7d4fae0088a354b41bed99d0c9d4d35a00f2c4
mahmoudali <mahmoudali@MAHMOUDALI>
2026-05-18 01:31:56 +0300

    push new src

---
+++ src/app.js
@@ -0,0 +1,7 @@
+// دالة تطبع نص يتم تمريره لها
+function printMessage(message) {
+    console.log("[App]: " + message);
+}
+
+// استدعاء الدالة لتجربتها
+printMessage("أهلاً بك! السكربت يعمل بنجاح.");
